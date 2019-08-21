4023abfdcda8dac8a961af5e41d08bd3842ccfbc
A0171206R <e0200776@u.nus.edu>
2019-08-21 13:06:32 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public Duke(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        System.out.println("____________________________________________________________\n");
+    }
+
+    static void printPartition() {
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public void functionBye(){
+        printPartition();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        printPartition();
+    }
+
+    public static void main(String[] args) {
+        Duke d = new Duke();
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        while(!(input.equals("bye"))) {
+            printPartition();
+            System.out.println(input);
+            printPartition();
+            input = s.nextLine();
+        }
+        d.functionBye();
     }
 }
